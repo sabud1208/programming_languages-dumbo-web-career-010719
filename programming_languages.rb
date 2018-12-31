5d82fd478910b59af22bb0ba68f46e71a6e51e23
Stephanie Messad <sabud1208@gmail.com>
2018-12-31 02:25:49 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,11 @@ require "pry"
 def reformat_languages(languages)
   new_hash = {}
 
-  languages.each do |style, description|
-    description.each do |k, v|
+  languages.each do |style, data|
+    data.each do |lang, type|
+      if new_hash.has_key?(lang)
+        new_hash[:style] << style
+        
         binding.pry
     end
   end
