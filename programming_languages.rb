3423830b03235663c400fdf089f78f4aec6c4587
Stephanie Messad <sabud1208@gmail.com>
2018-12-31 03:15:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,14 +2,14 @@ require "pry"
 
 def reformat_languages(languages)
   new_hash = {}
-
-  languages.each do |style, data|
-    data.each do |lang, type|
-      if new_hash.has_key?(lang)
-binding.pry
-      else
-        new_hash[lang] = type
-        new_hash[:style] = style
+  languages.each do |style, lang_hash|
+    lang_hash.each do |lang, type_hash|
+      type_hash.each do |sym, str|
+        if new_hash.include?(lang)
+          new_hash[lang][:style] << style
+        else
+          new_hash[lang] = {:type => str, :style => [style]}
+        end
       end
     end
   end
