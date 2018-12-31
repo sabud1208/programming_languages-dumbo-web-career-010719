ab37edd339aa7211660cd161a2316d538340f6a1
Stephanie Messad <sabud1208@gmail.com>
2018-12-31 02:34:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,8 +6,7 @@ def reformat_languages(languages)
   languages.each do |style, data|
     data.each do |lang, type|
       if new_hash.has_key?(lang)
-        new_hash[lang][style]
-        binding.pry
+        new_hash[lang][:style] << style
       else
         new_hash[lang] = type
         new_hash[:style] = style
